11ad2da57bc547f1c1faf9e39b3419c5ee90eba3
Jose García <josevk@hal-6000.(none)>
2011-03-27 20:41:09 +0200

    added accordeons for functions details and authentication feature

--- js/index.js
+++ js/index.js
@@ -1,12 +1,58 @@
  $(document).ready(function(){
+     initAccordeons();
   loadApiList();
+
+  
  });
+ 
+ 
  
  //global vars for the querys
  var actualApi;
  var actualVersion;
  var actualFunction;
  
+ function initAccordeons() {
+	  $('.accordeon_button').click(function() {
+
+        //REMOVE THE ON CLASS FROM ALL BUTTONS
+        $('.accordeon_button').removeClass('on');
+          
+        //NO MATTER WHAT WE CLOSE ALL OPEN SLIDES
+        $('.accordeon_content').slideUp('normal');
+   
+        //IF THE NEXT SLIDE WASN'T OPEN THEN OPEN IT
+        if($(this).next().is(':hidden') == true) {
+            
+            //ADD THE ON CLASS TO THE BUTTON
+            $(this).addClass('on');
+              
+            //OPEN THE SLIDE
+            $(this).next().slideDown('normal');
+         } 
+          
+     });
+      
+    
+    /*** REMOVE IF MOUSEOVER IS NOT REQUIRED ***/
+    
+    //ADDS THE .OVER CLASS FROM THE STYLESHEET ON MOUSEOVER 
+    $('.accordeon_button').mouseover(function() {
+        $(this).addClass('over');
+        
+    //ON MOUSEOUT REMOVE THE OVER CLASS
+    }).mouseout(function() {
+        $(this).removeClass('over');                                        
+    });
+    
+    /*** END REMOVE IF MOUSEOVER IS NOT REQUIRED ***/
+    
+    
+    /********************************************************************************************************************
+    CLOSES ALL S ON PAGE LOAD
+    ********************************************************************************************************************/   
+    $('.accordeon_content').hide();
+}
  
  function loadApiList() {
 	var div_api_selector=document.getElementById("API_content");
