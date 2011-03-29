ec2cdec1f4965753b9a6d8df1c6d5f48def427a2
Fernando Jiménez <ferjmoreno@gmail.com>
2011-03-29 16:50:48 +0200

    get_methods and get_services

--- selector/get_methods.php
+++ selector/get_methods.php
@@ -1,13 +1,11 @@
 <?php
 include_once('services.php');
 $services = new Services();
-$service_id = 1; // just for testing 
+$service_id = $_GET['service_id']; // just for testing 
 if($services)
     $methods = $services->getMethods($service_id);
-if(Constants::$debug) {
-    while($method = @mysql_fetch_object($methods)) 
-        echo $method->method_name. '<br>';
-}        
+while($method = @mysql_fetch_object($methods)) 
+        print "<div id=\"selector_row\" onclick=\"loadFormForFunction(".$method->method_id.")\"><p id=\"selector_value\">".$method->method_name."</p></div>";
 if($methods) mysql_free_result($methods);
 unset($services);
 ?>
--- selector/get_services.php
+++ selector/get_services.php
@@ -3,10 +3,8 @@ include_once('services.php');
 $services_o = new Services();
 if($services_o)
     $services = $services_o->getServices();
-if(Constants::$debug) {
-    while($service=@mysql_fetch_object($services)) 
-        echo $service->service_name. '<br>';
-}        
+while($service=@mysql_fetch_object($services)) 
+    print "<div id=\"selector_row\" onclick=\"loadApiFunctions(".$service->service_id.")\"><p id=\"selector_value\">".$service->service_name."</p></div>";
 if($services) mysql_free_result($services);
 unset($services_o);
 ?>
