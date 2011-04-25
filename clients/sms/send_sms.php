a19108e9ebe900b4965d60be167704de56a98740
Fernando Jiménez <ferjmoreno@gmail.com>
2011-04-25 14:21:33 +0200

    Modifications on send_sms

--- clients/sms/send_sms.php
+++ clients/sms/send_sms.php
@@ -1,19 +1,6 @@
 <?php
 include_once '../constants.php';
-
-$application_context = array(
-    'user' => array(
-        'token_access' => TOKEN,
-        'token_secret' => TOKEN_SECRET
-    ),
-    'app' => array(
-        'consumer_key' => CONSUMER_KEY,
-        'consumer_secret' => CONSUMER_SECRET        
-    )
-);
-
-$unica = new Unica($application_context);
-$sms = $unica->getApiSms();
+include_once 'common.php';
 
 $number =$_GET['address']; 
 $sms_message = $_GET['message'];
@@ -26,7 +13,7 @@ try {
 	if (!empty($id)) 
 	    echo '<p style="color:green"> SEND OK! SendSMS ID is: ' . $id . '</p>';
     echo '<p style="color:blue"> Request: ' . $unica->getLastRequest() .'</p>';
-    echo '<p style="color:blue"> Response: ' . $unica->getLastResponse() .'</p>';
+    echo '<p style="color:green"> Response: ' . $unica->getLastResponse() .'</p>';
 } catch(Exception $e) {
     echo '<p style="color:red"> ERROR! ' . $e->getMessage() . '</p>';
 }
